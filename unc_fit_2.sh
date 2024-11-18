48916c70093ee341228aa796c3ae5d1ddad1145f
Zach Calhoun <zcalhoun.nc@gmail.com>
2024-11-18 09:23:34 -0500

    Added two more experiments

--- unc_fit_2.sh
+++ unc_fit_2.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #SBATCH --job-name=unc_fit
-#SBATCH --output=unc_fit.out
-#SBATCH --error=unc_fit.err
+#SBATCH --output=unc_fit_2.out
+#SBATCH --error=unc_fit_2.err
 #SBATCH --mail-user=zachary.calhoun@duke.edu
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH -p carlsonlab-gpu
@@ -15,8 +15,8 @@ conda activate gpytorch
 
 python main.py \
     --file_path /datacommons/carlsonlab/zdc6/unc_data/data.csv \
-    --k 512 \
-    --training_batch_size 64 \
-    --checkpoint_path /cwork/zdc6/unc/k512_tb64/model/ \
-    --output_path /cwork/zdc6/unc/k512_tb64/ \
-    --lr 0.0001
+    --k 256 \
+    --training_batch_size 128 \
+    --checkpoint_path /cwork/zdc6/unc/k256_tb128/model/ \
+    --output_path /cwork/zdc6/unc/k256_tb128/ \
+    --lr 0.001
