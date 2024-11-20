11a77d3687cb81c1f6f30156992561fcd7a49c99
Zach Calhoun <zcalhoun.nc@gmail.com>
2024-11-19 20:03:53 -0500

    Added an additional experiment

---
+++ unc_fit_3.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#SBATCH --job-name=unc_fit_3
+#SBATCH --output=unc_fit_3.out
+#SBATCH --error=unc_fit_3.err
+#SBATCH --mail-user=zachary.calhoun@duke.edu
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH -p carlsonlab-gpu
+#SBATCH --gres=gpu:1
+#SBATCH --account=carlsonlab
+#SBATCH --mem=64G
+
+source ~/.bashrc
+conda activate gpytorch
+
+python main.py \
+    --file_path /datacommons/carlsonlab/zdc6/unc_data/data.csv \
+    --k 1024 \
+    --training_batch_size 32 \
+    --checkpoint_path /cwork/zdc6/unc/k512_tb64_periodic_features_2/model/ \
+    --output_path /cwork/zdc6/unc/k512_tb64_periodic_features_2/ \
+    --dataset UNC_periodic \
+    --lr 0.00001 \
+    --epochs 100 \
+    --noise_constraint 0.01 \
+    --model BaseVNNGP_PeriodicFeatures \
+    --time_multiplier 100.0
