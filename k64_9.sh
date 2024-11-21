09ce95a247758605edf48551b7edade9b6dda75e
Zach Calhoun <zcalhoun.nc@gmail.com>
2024-11-21 16:13:12 -0500

    Added k64_9 v2

--- k64_9.sh
+++ k64_9.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-#SBATCH --job-name=k64_5
-#SBATCH --output=k64_5.out
-#SBATCH --error=k64_5.err
+#SBATCH --job-name=k64_9
+#SBATCH --output=k64_9.out
+#SBATCH --error=k64_9.err
 #SBATCH --mail-user=zachary.calhoun@duke.edu
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH -p carlsonlab-gpu
@@ -17,11 +17,11 @@ python -u main.py \
     --file_path /datacommons/carlsonlab/zdc6/unc_data/data.csv \
     --k 64 \
     --batch_size 1024 \
-    --checkpoint_path /cwork/zdc6/unc/k64_5/model/ \
-    --output_path /cwork/zdc6/unc/k64_5/ \
+    --checkpoint_path /cwork/zdc6/unc/k64_9/model/ \
+    --output_path /cwork/zdc6/unc/k64_9/ \
     --dataset UNC_periodic \
     --lr 0.01 \
     --epochs 200 \
     --noise_constraint 0.01 \
     --model BaseVNNGP_PeriodicFeatures \
-    --time_multiplier 20.0
+    --time_multiplier 40.0
