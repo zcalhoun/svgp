e994c44351e6059929987a7673a664a8e15b141a
Zach Calhoun <zcalhoun.nc@gmail.com>
2025-03-29 13:35:25 -0400

    Updated era5 directory

--- scripts/preprocess/interp_era.sh
+++ scripts/preprocess/interp_era.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 python interp_era.py \
-    --input /work/zdc6/era/ \
+    --input /work/zdc6/era5/ \
     --output /work/zdc6/weather_underground/durham/era_by_station \
     --station_list /work/zdc6/weather_underground/durham/stations.csv \
     --test
