124de57cf38b582356d048312f39bbf44144e48c
Zach Calhoun <zcalhoun.nc@gmail.com>
2025-03-29 16:07:37 -0400

    Changed logic to organize era data

--- scripts/preprocess/interp_era.sh
+++ scripts/preprocess/interp_era.sh
@@ -10,4 +10,8 @@
 source ~/.bashrc
 conda activate era
 
-python test_interp_era.py
+python test_interp_era.py \
+    --input /work/zdc6/era5/ \
+    --output /work/zdc6/weather_underground/durham/era_by_month/ \
+    --station_list /work/zdc6/weather_underground/durham/stations.csv \
+    --test
