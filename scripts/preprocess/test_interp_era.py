4cab7ab99a2b6abfcd6c7e2741dad6d8f0e575bd
Zach Calhoun <zcalhoun.nc@gmail.com>
2025-03-29 15:56:01 -0400

    Test out the logic for getting the right file

--- scripts/preprocess/test_interp_era.py
+++ scripts/preprocess/test_interp_era.py
@@ -14,10 +14,18 @@ import xarray as xr
 
 def main():
 
-    # Get the slurm environmental variable for the task id
     task_id = os.getenv("SLURM_ARRAY_TASK_ID")
+    file = get_file(int(task_id))
+    print(file)
 
-    print(task_id)
+
+def get_file(task_id):
+    """
+    Get the file name from the task id.
+    """
+    date_range = pd.date_range(start="2019-01-01", end="2019-12-31", freq="M")
+    formatted_dates = date_range.strftime("%Y-%m").tolist()
+    return f"{formatted_dates[task_id]}.grib"
 
 
 if __name__ == "__main__":
