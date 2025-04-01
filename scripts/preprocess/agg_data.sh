1931dad5a88308878b6a521d6ed2d7ad0c356ef0
Zach Calhoun <zcalhoun.nc@gmail.com>
2025-04-01 16:38:27 -0400

    Updated code to add in all of Dec 2023

--- scripts/preprocess/agg_data.sh
+++ scripts/preprocess/agg_data.sh
@@ -4,5 +4,5 @@ python aggregate_station_data.py \
     --input /work/zdc6/weather_underground/weather_underground/station_data/ \
     --output /work/zdc6/weather_underground/durham/wu.csv \
     --start_date 2019-01-01 \
-    --end_date 2023-12-01 \
+    --end_date 2023-12-31 \
     --station_list /work/zdc6/weather_underground/durham/stations.csv \
