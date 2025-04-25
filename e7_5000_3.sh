5ad3acac79f622ec51b77e15ae3ddcaf921c3749
Zach Calhoun <zcalhoun.nc@gmail.com>
2025-04-24 21:05:23 -0400

    Added experiments for 5000 ip

---
+++ e7_5000_3.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#SBATCH --job-name=e7_5000_3
+#SBATCH --output=e7_5000_3.out
+#SBATCH --error=e7_5000_3.err
+#SBATCH --mail-user=zachary.calhoun@duke.edu
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH -p carlsonlab-gpu
+#SBATCH --gres=gpu:1
+#SBATCH --account=carlsonlab
+#SBATCH --mem=64G
+
+source gp/bin/activate
+
+python svgp_fit.py \
+    --data_directory /work/zdc6/weather_underground/durham/combined/ \
+    --output /work/zdc6/exp7/5000/3/ \
+    --num_epochs 100 \
+    --name e7_5000_3 \
+    --year 2023 \
+    --month 7 \
+    --likelihood Student \
+    --loss_function PLL \
+    --num_inducing_points 5000 \
+    --batch_size 4096 \
+    --lr 0.1
---
+++ e7_5000_4.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#SBATCH --job-name=e7_5000_4
+#SBATCH --output=e7_5000_4.out
+#SBATCH --error=e7_5000_4.err
+#SBATCH --mail-user=zachary.calhoun@duke.edu
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH -p carlsonlab-gpu
+#SBATCH --gres=gpu:1
+#SBATCH --account=carlsonlab
+#SBATCH --mem=64G
+
+source gp/bin/activate
+
+python svgp_fit.py \
+    --data_directory /work/zdc6/weather_underground/durham/combined/ \
+    --output /work/zdc6/exp7/5000/4/ \
+    --num_epochs 100 \
+    --name e7_5000_4 \
+    --year 2023 \
+    --month 7 \
+    --num_inducing_points 1000 \
+    --batch_size 4096 \
+    --lr 0.1
