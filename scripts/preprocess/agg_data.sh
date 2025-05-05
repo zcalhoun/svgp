66ecbf2db8cc65a185da2e96b57054b755ef211e
Zach Calhoun <zcalhoun.nc@gmail.com>
2025-05-05 09:01:03 -0400

    Added script to organize 2024 data

--- scripts/preprocess/agg_data.sh
+++ scripts/preprocess/agg_data.sh
@@ -2,7 +2,7 @@
 
 python aggregate_station_data.py \
     --input /work/zdc6/weather_underground/weather_underground/station_data/ \
-    --output /work/zdc6/weather_underground/durham/wu.csv \
-    --start_date 2019-01-01 \
-    --end_date 2023-12-31 \
+    --output /work/zdc6/weather_underground/durham/wu2024.csv \
+    --start_date 2024-01-01 \
+    --end_date 2024-12-31 \
     --station_list /work/zdc6/weather_underground/durham/stations.csv \
