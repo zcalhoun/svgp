01f4ab35fe52fd1026a87e31c3d75ffd7ec06878
Zach Calhoun <zcalhoun.nc@gmail.com>
2025-06-01 12:53:32 -0400

    Fixed a typo with ip1000

--- e10_500_p5.sh
+++ e10_500_p5.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-#SBATCH --job-name=e10_5
-#SBATCH --output=e10_5.out
-#SBATCH --error=e10_5.err
+#SBATCH --job-name=e10_5_500
+#SBATCH --output=e10_5_500.out
+#SBATCH --error=e10_5_500.err
 #SBATCH --mail-user=zachary.calhoun@duke.edu
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH -p carlsonlab-gpu
@@ -14,10 +14,10 @@ source gp/bin/activate
 
 python july24_experiments.py \
     --data_directory /work/zdc6/weather_underground/durham/july2024/ \
-    --output /work/zdc6/exp10/periodic/1000/5 \
+    --output /work/zdc6/exp10/periodic/500/5 \
     --num_epochs 100 \
     --name e10_5p \
-    --num_inducing_points 1000 \
+    --num_inducing_points 500 \
     --batch_size 512 \
     --likelihood Student \
     --loss_function IW-PLL \
