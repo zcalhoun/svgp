29bc619ddb49148e6af219b1776ad58b333490eb
Zach Calhoun <zcalhoun.nc@gmail.com>
2025-06-04 10:33:21 -0400

    Added more epochs to the experiments

--- e10_1000_p5.sh
+++ e10_1000_p5.sh
@@ -15,7 +15,7 @@ source gp/bin/activate
 python july24_experiments.py \
     --data_directory /work/zdc6/weather_underground/durham/july2024/ \
     --output /work/zdc6/exp10/periodic/1000/5b \
-    --num_epochs 10 \
+    --num_epochs 40 \
     --name e10_5p \
     --num_inducing_points 1000 \
     --batch_size 512 \
