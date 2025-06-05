3e7fdb9158c07fa67b4ab18a357dab425b8bf9ce
Zach Calhoun <zcalhoun.nc@gmail.com>
2025-06-05 10:56:38 -0400

    Updated script to agg 07/14/2024 data

--- scripts/preprocess/agg_data.sh
+++ scripts/preprocess/agg_data.sh
@@ -2,7 +2,7 @@
 
 python aggregate_station_data.py \
     --input /work/zdc6/weather_underground/weather_underground/station_data/ \
-    --output /work/zdc6/weather_underground/july2021_nc.csv \
-    --start_date 2021-07-01 \
-    --end_date 2021-07-31 \
+    --output /work/zdc6/weather_underground/july142024.csv \
+    --start_date 2024-07-14 \
+    --end_date 2024-07-14 \
     --station_list /work/zdc6/weather_underground/nc_stations.csv \
