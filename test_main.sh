a62249d55855e72d648e2183302add418dd70c4c
Zach Calhoun <zcalhoun.nc@gmail.com>
2025-06-09 11:48:56 -0400

    Going to recreate an error

--- test_main.sh
+++ test_main.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 #SBATCH --job-name=main
-#SBATCH --array=0-71
+#SBATCH --array=11 #0-71
 #SBATCH --mail-user=zachary.calhoun@duke.edu
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH -p scavenger-gpu
 #SBATCH --gres=gpu:1
 #SBATCH --mem=64G
 #SBATCH --output=./logs/output_%a.txt
-
+#SBATCH --error=./logs/output_%a.err
 
 source gp/bin/activate
 
