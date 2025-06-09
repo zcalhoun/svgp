362432421eedec0cb977e6918f81921ab42fd7b6
Zach Calhoun <zcalhoun.nc@gmail.com>
2025-06-09 10:55:37 -0400

    Testing out the first run of the script

---
+++ test_main.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#SBATCH --job-name=main
+#SBATCH --array=0
+#SBATCH --mail-user=zachary.calhoun@duke.edu
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH -p scavenger-gpu
+#SBATCH --gres=gpu:1
+#SBATCH --mem=64G
+#SBATCH --output=./logs/output_%A_%a.txt
+
+
+source gp/bin/activate
+
+python main.py \
+    -i /work/zdc6/weather_underground/durham/combined/ \
+    -o /work/zdc6/test_main/ \
+    --num_epochs 2
