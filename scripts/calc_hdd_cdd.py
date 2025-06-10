3f26dff54817bfa19461a07ba100ed4ca0719b52
Zach Calhoun <zcalhoun.nc@gmail.com>
2025-06-10 16:32:07 -0400

    Added scripts to calculate hdd and cdd

---
+++ scripts/calc_hdd_cdd.py
@@ -0,0 +1,56 @@
+"""
+This script simply loads the data and calculates the HDD and CDD for each month
+and year, and saves the results into a single CSV file.
+
+"""
+
+import os
+import argparse
+import pandas as pd
+
+
+def main(args):
+    """
+    Iterate through and load the data, calculate the HDD and CDD, and save the results.
+    """
+    all_dfs = []
+    for year in range(2019, 2025):
+        print(f"On year {year}")
+        for month in range(1, 13):
+            print(f"On month {month}")
+            df = pd.read_csv(os.path.join(args.ref_data, f"{year}-{month}.csv"))
+            df["hdd"] = (args.threshold - df["pred"]).clip(lower=0)
+            df["cdd"] = (df["pred"] - args.threshold).clip(lower=0)
+            sum_df = df.groupby(["lat", "lon"], as_index=False)[["hdd", "cdd"]].sum()
+            sum_df["hdd"] /= 24
+            sum_df["cdd"] /= 24
+            sum_df["year"] = year
+            sum_df["month"] = month
+            all_dfs.append(sum_df)
+
+    all_df = pd.concat(all_dfs, ignore_index=True)
+    output_file = os.path.join(args.output, "hdd_cdd.csv")
+    all_df.to_csv(output_file, index=False)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Calculate HDD and CDD from predictions."
+    )
+    parser.add_argument(
+        "--ref_data",
+        type=str,
+        required=True,
+        help="Path to the reference data directory.",
+    )
+    parser.add_argument(
+        "--output", type=str, required=True, help="Path to the output directory."
+    )
+    parser.add_argument(
+        "--threshold",
+        type=float,
+        default=18.3,
+        help="Temperature threshold for HDD/CDD calculation (default: 18.3 Celsius).",
+    )
+
+    main(parser.parse_args())
---
+++ scripts/hdd_cdd.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#SBATCH --job-name=hdd_cdd
+#SBATCH --ntasks=1
+#SBATCH --mem=64G
+#SBATCH --mail-user=zachary.calhoun@duke.edu
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --output=./cdd_hdd.txt
+#SBATCH --error=./cdd_hdd.err
+
+source ~/.bashrc
+conda activate era
+
+python calc_hdd_cdd.py \
+    --ref_data ref_data = "/work/zdc6/temp_preds/" \
+    --wu_file /work/zdc6/weather_underground/durham/ 
