ee5aadcc31348c5e757027745d6ccde7d5a80664
Zach Calhoun <zcalhoun.nc@gmail.com>
2025-06-10 14:54:11 -0400

    Fixed a typo with iterator

--- Trainers/validate.py
+++ Trainers/validate.py
@@ -24,7 +24,7 @@ def generate_maps(model, likelihood, test_loader):
     lower90 = []
     with torch.no_grad(), gpytorch.settings.num_likelihood_samples(1000):
 
-        for X in test_loader:
+        for (X,) in test_loader:
             if torch.cuda.is_available():
                 X = X.cuda()
 
