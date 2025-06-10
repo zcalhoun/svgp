ef82df4a0f241d72b2c82c49f8511b38fdfa999d
Zach Calhoun <zcalhoun.nc@gmail.com>
2025-06-10 16:39:46 -0400

    Fixed an error with the cdd/hdd arg

--- scripts/hdd_cdd.sh
+++ scripts/hdd_cdd.sh
@@ -12,5 +12,5 @@ source ~/.bashrc
 conda activate era
 
 python calc_hdd_cdd.py \
-    --ref_data ref_data = "/work/zdc6/temp_preds/" \
+    --ref_data /work/zdc6/temp_preds/ \
     --output /work/zdc6/weather_underground/durham/ 
