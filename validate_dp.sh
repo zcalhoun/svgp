71a933efee396c10b68f096c2691b05738dbc45f
Zach Calhoun <zcalhoun.nc@gmail.com>
2025-06-25 18:55:08 -0400

    Updated dewpoint experiment

--- validate_dp.sh
+++ validate_dp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #SBATCH --job-name=main
-#SBATCH --array=0
+#SBATCH --array=1-71
 #SBATCH --mail-user=zachary.calhoun@duke.edu
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH -p scavenger-gpu
