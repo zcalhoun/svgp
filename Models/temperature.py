7e3337c156d4aae2a8d3840e0cbeb230f3226460
Zach Calhoun <zcalhoun.nc@gmail.com>
2025-07-05 10:17:40 -0400

    one additional update to the kernel

--- Models/temperature.py
+++ Models/temperature.py
@@ -77,6 +77,15 @@ class TempModel(ApproximateGP):
                 gpytorch.kernels.MaternKernel(nu=0.5, active_dims=1)
                 * gpytorch.kernels.MaternKernel(nu=1.5, active_dims=6)
             )
+            + gpytorch.kernels.ScaleKernel(
+                gpytorch.kernels.RQKernel(
+                    nu=1.5,
+                    active_dims=(2, 3),
+                    ard_num_dims=2,
+                    lengthscale_prior=gpytorch.priors.SmoothedBoxPrior(0.01, 1.0),
+                )
+                * gpytorch.kernels.MaternKernel(nu=1.5, active_dims=6)
+            )
         )
 
     def forward(self, x):
