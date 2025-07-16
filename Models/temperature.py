fed7bec2fbd4ca903c20828536b2bdfc3215868f
Zach Calhoun <zcalhoun.nc@gmail.com>
2025-07-16 14:51:33 -0400

    updated to a mean field distribution

--- Models/temperature.py
+++ Models/temperature.py
@@ -7,7 +7,7 @@ import torch
 import gpytorch
 from gpytorch.models import ApproximateGP
 from gpytorch.variational import (
-    CholeskyVariationalDistribution,
+    MeanFieldVariationalDistribution,
     VariationalStrategy,
 )
 
@@ -25,7 +25,7 @@ class TempModel(ApproximateGP):
         Define the VNNGP model
         """
 
-        variational_distribution = CholeskyVariationalDistribution(
+        variational_distribution = MeanFieldVariationalDistribution(
             num_inducing_points=inducing_points.size(0)
         )
 
@@ -77,14 +77,15 @@ class TempModel(ApproximateGP):
                 gpytorch.kernels.MaternKernel(nu=0.5, active_dims=1)
                 * gpytorch.kernels.MaternKernel(nu=1.5, active_dims=6)
             )
-            # + gpytorch.kernels.ScaleKernel(
-            #     gpytorch.kernels.RQKernel(
-            #         active_dims=(2, 3),
-            #         ard_num_dims=2,
-            #         lengthscale_prior=gpytorch.priors.SmoothedBoxPrior(0.01, 1.0),
-            #     )
-            #     * gpytorch.kernels.MaternKernel(nu=1.5, active_dims=6)
-            # )
+            + gpytorch.kernels.ScaleKernel(
+                gpytorch.kernels.RQKernel(
+                    nu=1.5,
+                    active_dims=(2, 3),
+                    ard_num_dims=2,
+                    lengthscale_prior=gpytorch.priors.SmoothedBoxPrior(0.01, 1.0),
+                )
+                * gpytorch.kernels.MaternKernel(nu=1.5, active_dims=6)
+            )
         )
 
     def forward(self, x):
