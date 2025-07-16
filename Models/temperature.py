3ee7777ba38f7865e64abfea38792fba82ba4cf4
Zach Calhoun <zcalhoun.nc@gmail.com>
2025-07-16 16:27:02 -0400

    Reverted kernel

--- Models/temperature.py
+++ Models/temperature.py
@@ -59,7 +59,7 @@ class TempModel(ApproximateGP):
             gpytorch.kernels.ScaleKernel(
                 # change nu=0.5 to nu=1.5 on 7/3 -- this looks better
                 (
-                    alpha * gpytorch.kernels.MaternKernel(nu=1.5, active_dims=(4, 5))
+                    alpha * gpytorch.kernels.MaternKernel(nu=0.5, active_dims=(4, 5))
                     + alpha2
                 )
                 * gpytorch.kernels.MaternKernel(nu=1.5, active_dims=1)
