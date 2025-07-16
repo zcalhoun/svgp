837a0495df7d3f2b0224112c9e52769395c28405
Zach Calhoun <zcalhoun.nc@gmail.com>
2025-07-16 16:30:41 -0400

    Revert all

--- Models/temperature.py
+++ Models/temperature.py
@@ -59,7 +59,7 @@ class TempModel(ApproximateGP):
             gpytorch.kernels.ScaleKernel(
                 # change nu=0.5 to nu=1.5 on 7/3 -- this looks better
                 (
-                    alpha * gpytorch.kernels.MaternKernel(nu=0.5, active_dims=(4, 5))
+                    alpha * gpytorch.kernels.MaternKernel(nu=1.5, active_dims=(4, 5))
                     + alpha2
                 )
                 * gpytorch.kernels.MaternKernel(nu=1.5, active_dims=1)
@@ -73,19 +73,19 @@ class TempModel(ApproximateGP):
                 )
                 * gpytorch.kernels.MaternKernel(nu=1.5, active_dims=6)
             )
-            # + gpytorch.kernels.ScaleKernel(  # Added an extra term to capture random heat effect
-            #     gpytorch.kernels.MaternKernel(nu=0.5, active_dims=1)
-            #     * gpytorch.kernels.MaternKernel(nu=1.5, active_dims=6)
-            # )
-            # + gpytorch.kernels.ScaleKernel(
-            #     gpytorch.kernels.RQKernel(
-            #         nu=1.5,
-            #         active_dims=(2, 3),
-            #         ard_num_dims=2,
-            #         lengthscale_prior=gpytorch.priors.SmoothedBoxPrior(0.01, 1.0),
-            #     )
-            #     * gpytorch.kernels.MaternKernel(nu=1.5, active_dims=6)
-            # )
+            + gpytorch.kernels.ScaleKernel(  # Added an extra term to capture random heat effect
+                gpytorch.kernels.MaternKernel(nu=0.5, active_dims=1)
+                * gpytorch.kernels.MaternKernel(nu=1.5, active_dims=6)
+            )
+            + gpytorch.kernels.ScaleKernel(
+                gpytorch.kernels.RQKernel(
+                    nu=1.5,
+                    active_dims=(2, 3),
+                    ard_num_dims=2,
+                    lengthscale_prior=gpytorch.priors.SmoothedBoxPrior(0.01, 1.0),
+                )
+                * gpytorch.kernels.MaternKernel(nu=1.5, active_dims=6)
+            )
         )
 
     def forward(self, x):
