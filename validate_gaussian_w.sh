52da72692906833f7876a6b75090757135c08dba
Zach Calhoun <zcalhoun.nc@gmail.com>
2025-07-18 15:25:04 -0400

    Updated folder for results

--- validate_gaussian_w.sh
+++ validate_gaussian_w.sh
@@ -14,8 +14,8 @@
 source gp/bin/activate
 
 python main.py \
-    -i /work/zdc6/weather_underground/durham/combined/ \
-    -o /work/zdc6/temp_gauss_pll_weight/ \
+    -i /work-old/zdc6/weather_underground/durham/combined/ \
+    -o /work-old/zdc6/temp_gauss_pll_weight/ \
     --num_epochs 10 \
     --variable tempAvg \
     --likelihood Gaussian \
