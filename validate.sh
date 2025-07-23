e48bfec3c801cbb5b6e3ff0b980e9ca4d2aa8627
Zach Calhoun <zcalhoun.nc@gmail.com>
2025-07-23 10:16:01 -0400

    Validate script

--- validate.sh
+++ validate.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 #SBATCH --job-name=main
-#SBATCH --array=67
+#SBATCH --array=0-71
 #SBATCH --mail-user=zachary.calhoun@duke.edu
 #SBATCH --mail-type=BEGIN,END,FAIL
-#SBATCH -p scavenger-gpu
+#SBATCH -p carlsonlab-gpu
 #SBATCH --gres=gpu:1
 #SBATCH --mem=64G
 #SBATCH --output=./logs/output_%a.txt
@@ -15,8 +15,8 @@ source gp/bin/activate
 
 python main.py \
     -i /work-old/zdc6/weather_underground/durham/combined/ \
-    -o /work-old/zdc6/temp_stud_pll_w/ \
+    -o /work-old/zdc6/temp_stud_pll/ \
     --num_epochs 10 \
     --variable tempAvg \
     --likelihood Student \
-    --loss W-PLL
+    --loss PLL
