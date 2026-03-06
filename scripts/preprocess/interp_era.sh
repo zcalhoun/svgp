5236a0fc5852550576570b72569e7217bc0f580d
Zach Calhoun <zcalhoun.nc@gmail.com>
2026-03-06 10:21:51 -0500

    Updated era5 interpolation script

--- scripts/preprocess/interp_era.sh
+++ scripts/preprocess/interp_era.sh
@@ -12,6 +12,6 @@ source ~/.bashrc
 conda activate era
 
 python interp_era.py \
-    --input /work/zdc6/era5/ \
+    --input /datacommons/carlsonlab/zdc6/era5/ \
     --output /work/zdc6/weather_underground/durham/era_by_month/ \
     --station_list /work/zdc6/weather_underground/durham/stations.csv \
