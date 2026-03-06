e7e6f37e13934c8b539ed9775967ee9b1a7e8970
Zach Calhoun <zcalhoun.nc@gmail.com>
2026-03-06 10:10:00 -0500

    Updated station aggregation script

--- scripts/preprocess/agg_data.sh
+++ scripts/preprocess/agg_data.sh
@@ -8,11 +8,11 @@
 #SBATCH --error=./agg.err
 
 source ~/.bashrc
-conda activate era
+conda activate svgp
 
 
 python aggregate_station_data.py \
-    --input /work/zdc6/weather_underground/weather_underground/station_data/ \
+    --input /hpc/group/carlsonlab/weather_underground/station_data/ \
     --output /work/zdc6/weather_underground/durham/wu.csv \
     --start_date 2019-01-01 \
     --end_date 2024-12-31 \
