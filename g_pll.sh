6aba7b7494378833a26dc39b277a9fa1c9428634
Zach Calhoun <zcalhoun.nc@gmail.com>
2026-03-08 11:55:36 -0400

    Created G_PLL script

---
+++ g_pll.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#SBATCH --job-name=g_pll_2000
+#SBATCH --array=66
+#SBATCH --mail-user=zachary.calhoun@duke.edu
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH -p carlsonlab-gpu
+#SBATCH --gres=gpu:1
+#SBATCH --mem=64G
+#SBATCH --output=./logs_g_pll_2000/output_%a.txt
+#SBATCH --error=./logs_g_pll_2000/output_%a.err
+#SBATCH --time=01:30:00
+
+source ~/.bashrc
+conda activate svgp
+
+python main.py \
+    -i /work/zdc6/weather_underground/durham/combined/ \
+    -o /work/zdc6/temp_g_pll/ \
+    --num_epochs 40 \
+    --variable tempAvg \
+    --likelihood Gaussian \
+    --loss PLL \
+    --num_inducing_points 1000
